d7785c7815ce940c9e585cd5d0d0a648a7e69dd0
Edward Williams <edward.williams91@gmail.com>
2022-09-02 14:06:10 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,55 @@
 	<h2>PZ Server Mod Assistant</h2>
 	<p>This will help you organize the mods that you want enabled in your PZ server</p>
 	<!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-	{{demo}}
-	<label for="">Mod Name</label>
-	<input
-		class="bg-slate-200"
-		v-model="inputData.inputName"
-		type="text"
-	>
-	<label for="">Mod Id</label>
-	<button class="bg-slate-200">Add</button>
+	<div class="new-mod-form p-2">
+		<label for="">Mod Name</label>
+		<input
+			class="bg-slate-200 m-2"
+			v-model="inputData.inputName"
+			type="text"
+		>
+		<label for="">Mod Id</label>
+		<input
+			class="bg-slate-200 m-2"
+			v-model="inputData.inputId"
+			type="text"
+		>
+		<button
+			class="bg-slate-200 m-2"
+			@click="addMod"
+		>Add</button>
+	</div>
+
 	name={{inputData.inputName}}
+	<div class="mod-list-container">
+		<div
+			v-for="(mod,index) in modList"
+			:key="index"
+		>{{mod.name}}-{{mod.id}}</div>
+	</div>
+
 </template>
+
+
 
 <script>
 import { ref } from "vue";
 
 export default {
 	setup() {
-		const inputData = ref({ inputName: "demoName", inputId: "123" });
-		const demo = ref(0);
-		// expose to template and other options API hooks
+		const inputData = ref({ inputName: "", inputId: "" });
+		const modList = ref([]);
+		function addMod() {
+			modList.value.push({
+				name: inputData.value.inputName,
+				id: inputData.value.inputId,
+			});
+		}
+
 		return {
-			demo,
+			modList,
 			inputData,
+			addMod,
 		};
 	},
 
@@ -33,6 +59,4 @@ export default {
 </script>
 
 <style>
-#app {
-}
 </style>
